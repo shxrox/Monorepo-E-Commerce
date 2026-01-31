6a5f0a219a1248fef8a84f4a3c6c0e01a6a5c98f
shxrox <sharondeva8@gmail.com>
2026-01-31 19:59:39 +0530

    navbar logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link"
+import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <div className="" >Navbar</div>
+    <nav className="" >
+      {/* Left  */}
+        <Link href="/" className=""> 
+          <Image src="/logo.png" alt="TRENDLAMA-LOGO" width={36} height={36} className="w-6 md:w-9 md:h-9" />
+          <p className="text-md font-medium">
+            TRENDLAMA
+          </p>
+        </Link>
+      {/* Right  */}
+    </nav>
   )
 }
 
